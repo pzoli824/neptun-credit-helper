389f0683f495da9b7dce4ae46cf16e939e2bea88
Pribék Zoltán <pribekzoltan0824@gmail.com>
2023-11-01 11:17:47 +0100

    implemented tests for tree and fixed tree

--- pkg/models/tree.py
+++ pkg/models/tree.py
@@ -4,39 +4,46 @@ from typing import Generic, TypeVar, Optional, NamedTuple
 T = TypeVar("T")
 
 class Connections(NamedTuple):
-    parent: 'Optional[Node[T]]'
-    children: 'list[Node[T]]'
+    parent: Optional['Node[T]']
+    children: list['Node[T]']
 
 class Tree(Generic[T]):
 
     def __init__(self, data: T) -> None:
-        self.__node = Node(data, None)
-
-    def insert(self, data: T) -> None:
-        self.__node.insert(data)
+        self.__node = Node[T](data)
 
     @property
     def rootNode(self) -> 'Node[T]':
-        self.__node
+        return self.__node
+
+    def appendChildNodes(self, *nodes: 'Node[T]') -> None:
+        self.__node.appendChildNodes(*nodes)
 
     def getLeafNodes(self) -> 'list[Node[T]]':
-       return self.__findLeafNodes(self.__node.children, list[Node[T]])
+       return self.__findLeafNodes(self.__node.children[:], list[Node[T]]())
+
+    def getLeafNodesData(self) -> list[T]:
+        leafNodes = self.getLeafNodes()
+        return [node.data for node in leafNodes]
 
     def __findLeafNodes(self, children: 'list[Node[T]]', leafNodes: 'list[Node[T]]') -> 'list[Node[T]]':
-        if len(children) < 1:
+        if len(children) == 0:
             return leafNodes
         
-        first = children.pop(0)
-        children.extend(first.children)
+        firstNode = children.pop(0)
+        children.extend(firstNode.children)
 
-        self.__findLeafNodes(children, leafNodes)
+        if len(firstNode.children) == 0:
+            leafNodes.append(firstNode)
+
+        return self.__findLeafNodes(children, leafNodes)
 
 
 class Node(Generic[T]):
 
-    def __init__(self, data: T, parent: Optional['Node[T]']) -> None:
+    def __init__(self, data: T) -> None:
         self.__data = data
-        self.__connections = Connections(parent, list[Node[T]])
+        self.__connections = Connections(None, list[Node[T]]())
 
     @property
     def data(self) -> T:
@@ -46,8 +53,10 @@ class Node(Generic[T]):
     def data(self, data: T): 
         self.__data = data
 
-    def insert(self, data: T) -> T:
-        self.__connections.children.append(Node(data, self))
+    def appendChildNodes(self, *nodes: 'Node[T]') -> None:
+        for node in nodes:
+            node.parent = self
+            self.__connections.children.append(node)
 
     @property
     def parent(self):
@@ -55,8 +64,8 @@ class Node(Generic[T]):
     
     @parent.setter 
     def parent(self, parent: 'Node[T]'): 
-        self.__connections.parent = parent
+        self.__connections = Connections(parent, self.__connections.children)
 
     @property
-    def children(self) -> 'list[Node[T]]':
+    def children(self) -> list['Node[T]']:
         return self.__connections.children       
--- tests/models/test_tree.py
+++ tests/models/test_tree.py
@@ -1,31 +1,68 @@
 import pytest
 
-from pkg.models.tree import Tree
+from pkg.models.tree import Tree, Node
 
 class TestTree:
 
     def test_new_tree_initilaization_with_string(self):
         initValue = "for testing"
+
         t = Tree[str](initValue)
 
-        data = t.getFirstNode().getData()
+        assert initValue == t.rootNode.data
+
+    def test_tree_get_leaf_nodes(self):
+        t = Tree[str]("parent")
+        child1 = Node("child1")
+        child1_child1 = Node("child1_child1")
+        child1.appendChildNodes(child1_child1)
+
+        child2 = Node("child2")
+        child2_child1 = Node("child2_child1")
+        child2_child2 = Node("child2_child2")
+        child2.appendChildNodes(child2_child1, child2_child2)
+        child2_child2_chil1 = Node("child2_child2_chil1")
+        child2_child2.appendChildNodes(child2_child2_chil1)
+
+        expectedLeafNodeDatas = ["child1_child1", "child2_child1", "child2_child2_chil1"]
+
+        t.appendChildNodes(child1, child2)
         
-        assert initValue == data
+        assert expectedLeafNodeDatas == t.getLeafNodesData()
+        assert len(t.getLeafNodesData()) is 3
 
-    @pytest.mark.skip(reason="need to implement")
-    def test_tree_insert_new_element(self):
-        pass 
+class TestNode:
 
-class TestTreeNode:
-
-    @pytest.mark.skip(reason="need to implement")
     def test_new_tree_node_with_parent(self):
-        pass
+        p = Node("parent")
+        c = Node("child")
+        c2 = Node("child2")
 
-    @pytest.mark.skip(reason="need to implement")
+        p.appendChildNodes(c, c2)
+        c, c2 = p.children
+
+        assert c.parent is not None
+        assert c.parent.data is p.data
+        assert p.parent is None
+        assert c2.parent is not None
+        assert c2.parent.data is p.data
+
     def test_new_tree_node_without_parent(self):
-        pass
+        n = Node("init")
+        data = "node"
+        n.data = data
 
-    @pytest.mark.skip(reason="need to implement")
-    def test_tree_node_insert(self):
-        pass
+        assert n.parent is None
+        assert n.data is data
+
+    def test_append_child_node_to_tree_node(self):
+        p = Node("parent")
+        c1 = Node("child1")
+        c2 = Node("child2")
+        assert len(p.children) is 0
+
+        p.appendChildNodes(c1)
+        assert len(p.children) is 1
+
+        p.appendChildNodes(c2)
+        assert len(p.children) is 2
