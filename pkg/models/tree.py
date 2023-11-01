3b1f44731b39a9043c759dc8f422e1bef363539e
Pribék Zoltán <pribekzoltan0824@gmail.com>
2023-11-01 01:11:15 +0100

    implemed tree

--- pkg/models/tree.py
+++ pkg/models/tree.py
@@ -1,32 +1,62 @@
 
-from typing import Generic, TypeVar, Optional
+from typing import Generic, TypeVar, Optional, NamedTuple
 
 T = TypeVar("T")
+
+class Connections(NamedTuple):
+    parent: 'Optional[Node[T]]'
+    children: 'list[Node[T]]'
 
 class Tree(Generic[T]):
 
     def __init__(self, data: T) -> None:
-        self.__root = TreeNode[T](data, None)
-        self.__nodes: list['TreeNode[T]']
+        self.__node = Node(data, None)
 
     def insert(self, data: T) -> None:
-        node = TreeNode(data, self.__root)
-        self.__nodes.append(node)
+        self.__node.insert(data)
 
-    def getFirstNode(self) -> 'TreeNode[T]':
-        return self.__root     
+    @property
+    def rootNode(self) -> 'Node[T]':
+        self.__node
+
+    def getLeafNodes(self) -> 'list[Node[T]]':
+       return self.__findLeafNodes(self.__node.children, list[Node[T]])
+
+    def __findLeafNodes(self, children: 'list[Node[T]]', leafNodes: 'list[Node[T]]') -> 'list[Node[T]]':
+        if len(children) < 1:
+            return leafNodes
         
+        first = children.pop(0)
+        children.extend(first.children)
 
-class TreeNode(Generic[T]):
+        self.__findLeafNodes(children, leafNodes)
 
-    def __init__(self, data: T, parent: Optional['TreeNode[T]']) -> None:
-        self.__parent = parent
+
+class Node(Generic[T]):
+
+    def __init__(self, data: T, parent: Optional['Node[T]']) -> None:
         self.__data = data
-        self.__nodes = list[TreeNode[T]]
+        self.__connections = Connections(parent, list[Node[T]])
 
-    def insert(self, data: T) -> None:
-        node = TreeNode(data, self.__parent)
-        self.__nodes.append(node)
+    @property
+    def data(self) -> T:
+        return self.__data
+    
+    @data.setter 
+    def data(self, data: T): 
+        self.__data = data
 
-    def getData(self) -> T:
-        return self.__data   
+    def insert(self, data: T) -> T:
+        self.__connections.children.append(Node(data, self))
+
+    @property
+    def parent(self):
+        return self.__connections.parent  
+    
+    @parent.setter 
+    def parent(self, parent: 'Node[T]'): 
+        self.__connections.parent = parent
+
+    @property
+    def children(self) -> 'list[Node[T]]':
+        return self.__connections.children       
