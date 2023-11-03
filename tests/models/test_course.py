50d424303dcf189fdb25ee0f40b5078f450f631a
Pribék Zoltán <pribekzoltan0824@gmail.com>
2023-11-03 23:25:29 +0100

    created test file for course

---
+++ tests/models/test_course.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pkg.models.course import Course, ColumnsCourseField
+
+class TestCourse:
+
+    def test_course_initialization_by_constructor(self):
+        assert False
+
+    def test_course_initialization_by_static_method(self):
+        assert False        
