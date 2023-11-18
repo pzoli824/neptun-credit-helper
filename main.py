0da8c5a9e3f9895580c15b9bc626574bb1d55973
Pribék Zoltán <pribekzoltan0824@gmail.com>
2023-11-18 10:50:44 +0100

    added click to project

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import logging
 import traceback
+import click
 from pkg.providers.browser import BrowserFactory, BrowserType
 from pkg.providers.neptun import Neptun, University
+from pkg.ui.terminal import UITerminal
 
-browser = BrowserFactory.create_browser(BrowserType.CHROME)
-driver = browser.driver
+@click.group()
+def cli():
+    pass
 
-neptun = Neptun(browser, University.SZTE)
-neptun.login("username", "password")
-try:
-    neptun.get_all_course_informations()
-    neptun.get_enrolled_courses_in_current_semester()
+@cli.command()
+def run():
+    '''Runs the application in production mode'''
+    browser = BrowserFactory.create_browser(BrowserType.CHROME)
+    driver = browser.driver
 
-except Exception as e:
-    logging.error(traceback.format_exc())
+    neptun = Neptun(browser, University.SZTE)
+    neptun.login("username", "password")
+    try:
+        neptun.get_all_course_informations()
+        neptun.get_enrolled_courses_in_current_semester()
 
-finally:
-    neptun.logout_and_quit()
+    except Exception as e:
+        logging.error(traceback.format_exc())
 
+    finally:
+        neptun.logout_and_quit()
+
+
+@cli.command()
+def test_run():
+    '''Runs the application in test mode, with test data'''
+    click.echo('test run')
+    ui = UITerminal()
+
+
+if __name__ == '__main__':
+    cli()
