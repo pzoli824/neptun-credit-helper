1b5f351794d44f5358c739cf1ae5eee7e8502024
Pribék Zoltán <pribekzoltan0824@gmail.com>
2024-01-13 07:44:23 +0100

    changed logic for program entry point

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import logging
-import traceback
 import click
+import sys
+from data.student import data_test_student
+from pkg.models.student import Student
 from pkg.providers.browser import BrowserFactory, BrowserType
-from pkg.providers.neptun import Neptun, University
+from pkg.providers.neptun import Neptun
 from pkg.ui.terminal import UITerminal
+
+sys.setrecursionlimit(2000)
 
 @click.group()
 def cli():
@@ -13,26 +17,32 @@ def cli():
 def run():
     '''Runs the application in production mode'''
     browser = BrowserFactory.create_browser(BrowserType.CHROME)
-    driver = browser.driver
 
-    neptun = Neptun(browser, University.SZTE)
-    neptun.login("username", "password")
+    credentials = UITerminal.get_login_credentials()
+    neptun = Neptun(browser, credentials.university)
+    neptun.login(credentials.username, credentials.password)
+
+    student = Student("test name")
+    #TODO get student name too
     try:
-        neptun.get_all_course_informations()
-        neptun.get_enrolled_courses_in_current_semester()
+        student.all_courses = neptun.get_all_course_informations()
+        student.current_courses = neptun.get_enrolled_courses_in_current_semester()
 
     except Exception as e:
-        logging.error(traceback.format_exc())
+        logging.error(e)
 
     finally:
         neptun.logout_and_quit()
+        ui = UITerminal(student)
+        ui.home()
+
 
 
 @cli.command()
 def test_run():
     '''Runs the application in test mode, with test data'''
-    click.echo('test run')
-    ui = UITerminal()
+    ui = UITerminal(data_test_student())
+    ui.home()
 
 
 if __name__ == '__main__':
